b7a9a90dbff50ad1be8867a0f0038f0aea1cd742
Arman Radmanesh <Radmanesh@gmail.com>
2024-01-25 15:35:29 -0600

    Added missing end-points

--- src/admin/utils/sensorsDownload.js
+++ src/admin/utils/sensorsDownload.js
@@ -249,6 +249,272 @@ const downloadSensors = [
       { name: 'end-date', type: 'date', defaultValue: '2024-01-01' },
     ],
     parameters: []
+  },
+  {
+    id:"AZM_Time_Series_byInterval",
+    label:"Get AZM Time Series by Interval (no Intraday)",
+    link: "/1/user/[user-id]/activities/active-zone-minutes/date/[start-date]/[end-date].json",
+    description: "The number of minutes spent in each activity zone during the given day. The response includes activity log entries for the specified day.",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date',  defaultValue: '2024-01-01' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: []
+  },
+  {
+    id:"AZM_Time_Series_byDate",
+    label:"Get AZM Time Series by Date (no Intraday)",
+    description: "The number of minutes spent in each activity zone during the given day. The response includes activity log entries for the specified day.",
+    link: "/1/user/[user-id]/activities/active-zone-minutes/date/[date]/[period].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'date', type: 'date',  defaultValue: '2024-01-01' },
+      { name: 'period', type: 'select', values: ['1d', '7d', '30d', '1w', '1m'], defaultValue: '1d' }
+    ],
+    parameters: []
+  },
+  {
+    id:"Activity_Lifetime_Stats",
+    label: "Get Lifetime Stats",
+    description: "Retrieves the user's activity statistics. Retrieves the user's activity statistics in the format requested using units in the unit system which corresponds to the Accept-Language header provided.",
+    link: "/1/user/[user-id]/activities.json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' }
+    ],
+    parameters: []
+  },
+  {
+    id:"Activity_Log_List",
+    label: "Get Activity Log List",
+    description: "Retrieves a list of a user's activity log entries before or after a given day.",
+    link: "/1/user/[user-id]/activities/list.json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' }
+    ],
+    parameters: [
+      { name: 'beforeDate', type: 'date', defaultValue: '2024-01-01' },
+      { name: 'sort', type: 'hidden', defaultValue: 'desc' },
+      { name: 'offset', type: 'number', defaultValue: 0 },
+      { name: 'limit', type: 'number', defaultValue: 100 }
+    ]
+  },
+  {
+    id:"Get_Activity_Tcx",
+    label: "Get Activity TCX",
+    description: "Retrieves a user's activity details in TCX format.",
+    link: "/1/user/[user-id]/activities/[log-id].tcx",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'log-id', type: 'text', defaultValue: '' }
+    ],
+    parameters: [
+      { name: 'includePartialTCX', type: 'boolean', defaultValue: true }
+    ]
+  },
+  {
+    id:"Get_Daily_Activity_Summary",
+    label: "Get Daily Activity Summary",
+    description: "Retrieves a summary and list of a user's activities and activity log entries for a given day.",
+    link: "/1/user/[user-id]/activities/date/[date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'date', type: 'date', defaultValue: 'today' }
+    ],
+    parameters: [
+    ]
+  },
+  {
+    id:"Get_Activity_steps_Time_Series_by_Date_Range",
+    label: "Get Activity Time Series of steps by Date Range (no Intraday)",
+    description: "retrieves the activity time series data for steps resource on a date range.",
+    link: "/1/user/[user-id]/activities/steps/date/[start-date]/[end-date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date', defaultValue: '2023-01-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_Activity_activityCalories_Time_Series_by_Date_Range",
+    label: "Get Activity Time Series of activityCalories by Date Range (no Intraday)",
+    description: "retrieves the activity time series data for activityCalories resource on a date range.",
+    link: "/1/user/[user-id]/activities/activityCalories/date/[start-date]/[end-date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date', defaultValue: '2023-12-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_Activity_calories_Time_Series_by_Date_Range",
+    label: "Get Activity Time Series of calories  by Date Range (no Intraday)",
+    description: "retrieves the activity time series data for calories resource on a date range.",
+    link: "/1/user/[user-id]/activities/calories/date/[start-date]/[end-date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date', defaultValue: '2023-01-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_Activity_caloriesBMR_Time_Series_by_Date_Range",
+    label: "Get Activity Time Series of caloriesBMR by Date Range (no Intraday)",
+    description: "retrieves the activity time series data for caloriesBMR resource on a date range.",
+    link: "/1/user/[user-id]/activities/caloriesBMR/date/[start-date]/[end-date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date', defaultValue: '2023-01-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_Activity_distance_Time_Series_by_Date_Range",
+    label: "Get Activity Time Series of distance by Date Range (no Intraday)",
+    description: "retrieves the activity time series data for distance resource on a date range.",
+    link: "/1/user/[user-id]/activities/distance/date/[start-date]/[end-date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date', defaultValue: '2023-01-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_Activity_elevation_Time_Series_by_Date_Range",
+    label: "Get Activity Time Series of elevation by Date Range (no Intraday)",
+    description: "retrieves the activity time series data for elevation resource on a date range.",
+    link: "/1/user/[user-id]/activities/elevation/date/[start-date]/[end-date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date', defaultValue: '2023-01-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_Activity_floors_Time_Series_by_Date_Range",
+    label: "Get Activity Time Series of floors by Date Range (no Intraday)",
+    description: "retrieves the activity time series data for floors resource on a date range.",
+    link: "/1/user/[user-id]/activities/floors/date/[start-date]/[end-date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date', defaultValue: '2023-01-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_Activity_minutesSedentary_Time_Series_by_Date_Range",
+    label: "Get Activity Time Series of minutesSedentary by Date Range (no Intraday)",
+    description: "retrieves the activity time series data for minutesSedentary resource on a date range.",
+    link: "/1/user/[user-id]/activities/minutesSedentary/date/[start-date]/[end-date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date', defaultValue: '2023-01-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_Activity_minutesLightlyActive_Time_Series_by_Date_Range",
+    label: "Get Activity Time Series of minutesLightlyActive by Date Range (no Intraday)",
+    description: "retrieves the activity time series data for minutesLightlyActive resource on a date range.",
+    link: "/1/user/[user-id]/activities/minutesLightlyActive/date/[start-date]/[end-date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date', defaultValue: '2023-01-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_Activity_minutesFairlyActive_Time_Series_by_Date_Range",
+    label: "Get Activity Time Series of minutesFairlyActive by Date Range (no Intraday)",
+    description: "retrieves the activity time series data for minutesFairlyActive resource on a date range.",
+    link: "/1/user/[user-id]/activities/minutesFairlyActive/date/[start-date]/[end-date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date', defaultValue: '2023-01-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_Activity_minutesVeryActive_Time_Series_by_Date_Range",
+    label: "Get Activity Time Series of minutesVeryActive by Date Range (no Intraday)",
+    description: "retrieves the activity time series data for minutesVeryActive resource on a date range.",
+    link: "/1/user/[user-id]/activities/minutesVeryActive/date/[start-date]/[end-date].json",
+    arguments: [
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date', defaultValue: '2023-01-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_Breathing_Rate_Summary_by_Interval",
+    label:"Get Breathing Rate Summary by Interval (30 days)",
+    description:"This endpoint returns average breathing rate data for a date range. Breathing Rate data applies specifically to a user's <main sleep>, which is the longest single period of time during which they were asleep on a given date.",
+    link:"/1/user/[user-id]/br/date/[start-date]/[end-date].json",
+    arguments:[
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date',  defaultValue: '2023-12-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ], 
+    parameters:[]
+  },
+  {
+    id:"Get_Heart_Rate_Time_Series_by_Date_Range",
+    label:"Get Heart Rate Time Series by Date Range",
+    description:"Retrieves the heart rate time series data over a period of time by specifying a date range. The response will include only the daily summary values.",
+    link:"/1/user/[user-id]/activities/heart/date/[start-date]/[end-date].json",
+    arguments:[
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date',  defaultValue: '2023-01-10' },
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01' }
+    ],
+    parameters: [
+      { name: 'timezone', type: 'select', values: ['UTC','America/New_York','America/Chicago','America/Denver','America/Los_Angeles'], defaultValue: 'UTC' }
+    ]
+  },
+  {
+    id:"Get_HRV_Summary_by_Interval",
+    label:"Get HRV Summary by Interval",
+    description:" This endpoint returns the Heart Rate Variability (HRV) data for a date range. HRV data applies specifically to a user's <main sleep>, which is the longest single period of time asleep on a given date.",
+    link:"/1/user/[user-id]/hrv/date/[start-date]/[end-date].json",
+    arguments:[
+      { name: 'user-id', type: 'text', defaultValue: '-' },
+      { name: 'start-date', type: 'date',  defaultValue: '2023-12-10'},
+      { name: 'end-date', type: 'date', defaultValue: '2024-01-01'}
+    ],
+    parameters:[]
   }
 ];
 
