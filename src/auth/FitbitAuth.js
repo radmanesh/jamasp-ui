4cbaaa46bac4a9d2adce86d6f663ef94289aef91
Arman Radmanesh <Radmanesh@gmail.com>
2024-02-10 13:52:04 -0600

    refactored file

--- src/auth/FitbitAuth.js
+++ src/auth/FitbitAuth.js
@@ -2,138 +2,106 @@
  * This file contains the implementation of Fitbit authentication functionality.
  * It includes functions for generating the Fitbit login URL, retrieving the Fitbit authentication state for a user,
  * and exchanging the authorization code for tokens.
- * 
+ *
  * @module FitbitAuth
  * @filepath /Users/armanrad/Documents/Projects/Work/Cut/cutsocial/jamasp-ui/src/auth/FitbitAuth.js
  */
-import { Timestamp, collection, deleteField, doc, getDocs, query, updateDoc, where } from "firebase/firestore";
-import { db } from "../firebase";
-import {  storeFitbitToken } from './FitbitAuthUtils';
-import { getOrRenewAccessToken } from "./api";
+import { Timestamp, collection, deleteField, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
+import { db } from '../firebase';
+import { storeFitbitToken } from './FitbitAuthUtils';
+import { getOrRenewAccessToken } from './api';
 
-export const authorizationEndpoint = 'https://www.fitbit.com/oauth2/authorize';
+const authorizationEndpoint = 'https://www.fitbit.com/oauth2/authorize';
+
 /**
  * Removes a device for a specific user.
- * @param {string} userId - google user.id.
- * @param {string} deviceId - fitibit user_id. The ID of the device to be removed.
+ * @param {string} userId - Google user ID.
  * @returns {Promise<Object|void>} - A promise that resolves to the token object if it exists and is valid, or void if there is an error or no token.
  */
 async function removeDevice(userId) {
   try {
-    const q = query(collection(db, "users"), where("uid", "==", userId));
-    const querySnapshot = await getDocs(q);
-    var docRef;
+    const querySnapshot = await getDocs(query(collection(db, 'users'), where('uid', '==', userId)));
     if (querySnapshot.size === 0) {
-      // the user has nev
-      console.error("No such document!", userId);
-      return null;
-    } else {
-      docRef = querySnapshot.docs[0];
+      throw new Error('No such document!');
     }
+    const docRef = querySnapshot.docs[0];
     const usr = docRef.data();
-    //console.log("usr", usr);
     if (!usr) {
-      console.error("userObj is null");
-      return null;
-    } else {
-      // const { fitbitData, ...newUser } = usr;
-      // console.log("new", newUser);
-
-      const userRef = doc(db, 'users', docRef.id);
-      const updaredUesr = await updateDoc(userRef, {fitbitData:deleteField()});
-      console.log("new", updaredUesr);
+      throw new Error('userObj is null');
     }
+    const userRef = doc(db, 'users', docRef.id);
+    await updateDoc(userRef, { fitbitData: deleteField() });
   } catch (e) {
-    console.error("Error adding document: ", e);
-    return null;
-  }
-}
-
-const refreshFitbitToken = function(userId) {
-  try {
-    const q = query(collection(db, "users"), where("uid", "==", userId));
-    getDocs(q).then((querySnapshot) => {
-      var docRef;
-      if (querySnapshot.size === 0) {
-        // the user has nev
-        console.log("No such document!");
-        return null;
-      } else {
-        docRef = querySnapshot.docs[0];
-      }
-      const usr = docRef.data();
-      console.log("usr", usr);
-      if (!usr) {
-        console.error("userObj is null");
-        return null;
-      } else {
-        const token = usr.fitbitData;
-        getOrRenewAccessToken('renew', token.refresh_token, token.code_verifier).then((newToken) => {
-          if (newToken?.access_token) {
-            storeFitbitToken(userId, newToken).then((res) => {
-              console.log("newToken is stored" , res);
-              return newToken;
-            });;
-          } else {  
-            console.log("newToken is null");
-          }
-        }).catch((err) => {
-          console.error("Error getting user data: ", err);
-        });
-        return null;
-      }
-    }).catch((err) => {
-      console.error("Error adding document: ", err);
-      return null;
-    });
-
-  } catch (e) {
-    console.error("Error adding document: ", e);
+    console.error('Error removing device: ', e);
     return null;
   }
 }
 
 /**
- * Retrieves the Fitbit authentication state for a given user. first checks database has the fitbit token. if no token is available it should redirect to the fitbit login page. and if it needs refresh it should (better to do it in another module) refresh the token and store it in the database.
- * @param {string} userId - The ID of the google user.
+ * Refreshes the Fitbit access token for a given user.
+ * @param {string} userId - Google user ID.
+ * @returns {Promise<Object|null>} - A promise that resolves to the new Fitbit access token if the refresh is successful, or null if there is an error.
+ */
+async function refreshFitbitToken(userId) {
+  try {
+    const querySnapshot = await getDocs(query(collection(db, 'users'), where('uid', '==', userId)));
+    if (querySnapshot.size === 0) {
+      throw new Error('No such document!');
+    }
+    const docRef = querySnapshot.docs[0];
+    const usr = docRef.data();
+    if (!usr) {
+      throw new Error('userObj is null');
+    }
+    const token = usr.fitbitData;
+    const newToken = await getOrRenewAccessToken('renew', token.refresh_token, token.code_verifier);
+    if (newToken?.access_token) {
+      await storeFitbitToken(userId, newToken);
+      console.log('newToken is stored');
+      return newToken;
+    } else {
+      console.log('newToken is null');
+      return null;
+    }
+  } catch (err) {
+    console.error('Error refreshing token: ', err);
+    return null;
+  }
+}
+
+/**
+ * Retrieves the Fitbit authentication state for a given user.
+ * @param {string} userId - Google user ID.
  * @returns {Promise<Object|null>} - A promise that resolves to the Fitbit access token if the user has logged in with Fitbit, or null if the user has never logged in with Fitbit.
  */
-const getFitbitAuthState = async function (userId) {
-  //console.log(userId);
+async function getFitbitAuthState(userId) {
   try {
-    const q = query(collection(db, "users"), where("uid", "==", userId));
-    const querySnapshot = await getDocs(q);
-    var docRef;
+    const querySnapshot = await getDocs(query(collection(db, 'users'), where('uid', '==', userId)));
     if (querySnapshot.size === 0) {
-      // the user has nev
-      console.log("No such document!");
-      return;
-    } else {
-      docRef = querySnapshot.docs[0];
+      throw new Error('No such document!');
     }
+    const docRef = querySnapshot.docs[0];
     const token = docRef.data().fitbitData;
-    console.log("getFitbitAuthState, token: ", token);
-    // check if user has logged in with fitbit
+    console.log('getFitbitAuthState, token: ', token);
     if (!token) {
-      // user has never logged in with fitbit, maybe? we should redirect to the fitbit login page. 
-      console.log("No fitbit token");
-      return;
+      console.log('No Fitbit token');
+      return null;
     } else if (token.user_id) {
-      //we should check here if there is a token and it is valid and not expired. if it is expired we should refresh it.
-      if(checkTokenExpiration(token)){
-        console.log("token is expired");
+      if (checkTokenExpiration(token)) {
+        console.log('Token is expired');
         const newToken = await getOrRenewAccessToken('renew', token.refresh_token, token.code_verifier);
-        if(newToken?.access_token)
-          storeFitbitToken(userId, newToken);
-        else
-          console.log("newToken is null");
+        if (newToken?.access_token) {
+          await storeFitbitToken(userId, newToken);
+        } else {
+          console.log('newToken is null');
+        }
       }
       console.log(token);
       return token;
     }
   } catch (e) {
-    console.error("Error adding document: ", e);
-    return;
+    console.error('Error getting Fitbit auth state: ', e);
+    return null;
   }
 }
 
@@ -144,10 +112,10 @@ const getFitbitAuthState = async function (userId) {
  */
 function checkTokenExpiration(token) {
   let expired = false;
-  if(token?.timestamp === undefined || token?.expires_in === undefined || token?.timestamp === null || token?.expires_in === null) {
+  if (token?.timestamp === undefined || token?.expires_in === undefined || token?.timestamp === null || token?.expires_in === null) {
     expired = true;
-  }else{
-    if(token.timestamp.seconds+token.expires_in < Timestamp.now().seconds) {
+  } else {
+    if (token.timestamp.seconds + token.expires_in < Timestamp.now().seconds) {
       expired = true;
     }
   }
@@ -161,18 +129,23 @@ function checkTokenExpiration(token) {
  * @returns {Promise<any>} - A promise that resolves to the access code.
  */
 async function exchangeCodeForTokens(code, code_verifier) {
-  // Obtain the authorization code from the URL query parameters
-  // const code = new URLSearchParams(window.location.search).get('code');
-  if (code) {
-    // Send a POST request to exchange the authorization code for tokens
+  try {
+    if (!code) {
+      throw new Error('Invalid authorization code');
+    }
     console.log(code);
     const getAccessCode = await getOrRenewAccessToken('get', code, code_verifier);
     console.log(getAccessCode);
     return getAccessCode;
-    // Redirect to home page
-    // window.location.replace('/');
+  } catch (err) {
+    console.error('Error exchanging code for tokens: ', err);
+    return null;
   }
 }
 
-export { exchangeCodeForTokens, getFitbitAuthState, removeDevice, refreshFitbitToken };
-
+export {
+  exchangeCodeForTokens,
+  getFitbitAuthState,
+  removeDevice,
+  refreshFitbitToken,
+};
